1f82645821b799ea9628eb92f1383e69e161fdd0
Oski_Home <david2005prieto@gmail.com>
2026-05-30 17:22:46 -0500

    FPGA testeada y LED de output valido

--- hardware/quartus/video_vga_system.v
+++ hardware/quartus/video_vga_system.v
@@ -1,6 +1,9 @@
 module video_vga_system (
-    input wire CLOCK_50
+    input wire CLOCK_50,
+    output wire LEDR0
 );
+
+    assign LEDR0 = 1'b1; // Turn on LEDR0 to indicate the system is powered
 
     video_system u0 (
         .clk_clk(CLOCK_50)
